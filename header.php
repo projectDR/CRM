a476b1eeb86831e71ed9b0b08df8d5cfb31542d1
Unknown <greatspy@mail.ru>
2017-05-08 15:48:13 +0500

    dima
    normal header

--- header.php
+++ header.php
@@ -1,8 +1,14 @@
 <header>
+
     <nav class="navbar navbar-default navbar-fixed-top"" role="navigation">
     <div class="container-fluid">
-        <button type="button" class="btn btn-default navbar-btn">Войти</button>
-        <button type="button" class="btn btn-default navbar-btn">Зарегестрироваться</button>
+        <ul class="nav nav-pills">
+            <!--Эти ссылки будут выводиться для авторизованного исполнителя -->
+            <li><a href="#">Профиль</a></li>
+            <li><a href="#" onclick="getApplList()">Список заявок</a></li>
+        <button type="button" class="btn btn-default navbar-btn" id="log-in-button">Войти</button>
+        <button type="button" class="btn btn-default navbar-btn" id="log-up-button">Зарегестрироваться</button>
+        </ul>
     </div>
     </nav>
 </header>
